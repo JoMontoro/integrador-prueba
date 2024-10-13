452db11be7c0cb240748bc9ee93f9323f0092a4c
hp <hp@DESKTOP-DILREOG>
2024-10-13 11:57:31 -0500

    nuevog

---
+++ src/main/java/com/example/integrador_prueba/IntegradorPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.integrador_prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradorPruebaApplication.class, args);
+	}
+
+}
