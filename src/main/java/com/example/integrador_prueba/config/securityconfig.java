94c86761646319d09d7f4ec4d4f619703c4a82b1
hp <hp@DESKTOP-DILREOG>
2024-10-13 12:10:35 -0500

    fytfyddc

--- src/main/java/com/example/integrador_prueba/config/securityconfig.java
+++ src/main/java/com/example/integrador_prueba/config/securityconfig.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.example.integrador_prueba.config;
 
 import org.springframework.context.annotation.Bean;
